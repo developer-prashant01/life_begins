cf652c770e7f16a61d1e8231a3674b513b47b20a
developer-prashant01 <155139634+developer-prashant01@users.noreply.github.com>
2023-12-30 07:40:11 +0000

    container config done

---
+++ 01_basics/variables.js
@@ -0,0 +1,22 @@
+let accNumber=50
+console.log(accNumber)
+
+accNumber=100
+console.log("account number" + " "+accNumber)
+
+accNumber=500
+console.log(accNumber)
+
+const  accountId="prashant gupta"
+console.log(accountId)
+
+// accountId="prashant Kumar gupta"
+// console.log(accountId)
+
+
+let accountCity="gwalior"
+console.log(accountCity)
+
+
+accountCity="Jabalpur"
+console.log(accountCity)
