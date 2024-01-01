6f6038088a91f638acb4d01a2ee6479fdf485979
developer-prashant01 <155139634+developer-prashant01@users.noreply.github.com>
2024-01-01 07:26:36 +0000

    datatypes

---
+++ 01_basics/variable2.js
@@ -0,0 +1,11 @@
+let fName="Prashant";
+let lName="Gupta"
+let age=19
+//let isLogged=True // always assingn as true
+let isLogged=true
+
+//console.log(typeof(fName))
+console.table([typeof(fName),typeof(lName),typeof(age),typeof(isLogged),typeof(null),typeof(undefined)])
+window.alert("Hello World");
+
+
