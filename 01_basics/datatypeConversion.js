348d15da3eb2b62e6a2fc818da970e58c97f0503
developer-prashant01 <155139634+developer-prashant01@users.noreply.github.com>
2024-01-01 09:51:58 +0000

    datatype conversions

---
+++ 01_basics/datatypeConversion.js
@@ -0,0 +1,79 @@
+/*
+
+let score="33"
+console.log(typeof score)
+
+let numberScore=Number(score)
+score=Number(score)
+console.log(typeof score) 
+
+*/
+
+/*
+
+let score="33abc"
+console.log(typeof score +"  "+ score )
+
+let numberScore=Number(score)
+console.log(typeof numberScore +" "+ numberScore)
+// score=Number(score)
+console.log(numberScore) 
+
+*/
+
+/*
+let score=null
+console.log(score)
+
+let numberScore=Number(score)
+console.log(numberScore +" "+ typeof numberScore)
+
+// let numberScore=String(score)
+// console.log(numberScore +" "+ typeof numberScore)
+*/
+
+/*
+
+let score=undefined
+
+console.log(score+" "+typeof score)
+
+let numberScore=Number(score)
+console.log(numberScore+" "+typeof numberScore)
+
+*/
+
+
+
+/*
+
+let score=1;
+console.log(typeof(score))
+
+let booleanScore=Boolean(score)
+console.log(booleanScore+" "+typeof booleanScore)
+*/
+
+/*
+
+let name="pras"
+console.log(name+ " " + typeof(name))
+
+let stringName=Number(name)
+console.log(stringName+ " " + typeof(stringName))
+
+*/
+
+//let score=""  //o/p: false
+let score="prashant"  //o/p: true
+console.log(typeof score)
+let booleanScore=Boolean(score)
+console.log(booleanScore +" "+ typeof booleanScore)
+
+
+
+
+
+
+
+
